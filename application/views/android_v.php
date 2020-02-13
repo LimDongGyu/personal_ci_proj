f54df17aaefd1e8b53b62fca7083c9f3299f7ac1
ldg <limdg5335@gmail.com>
2020-02-14 01:54:29 +0900

    update: ul li tag option

--- application/views/android_v.php
+++ application/views/android_v.php
@@ -8,23 +8,49 @@
     <script src='//unpkg.com/popper.js@1/dist/umd/popper.min.js'></script>
     <script src='//unpkg.com/bootstrap@4/dist/js/bootstrap.min.js'></script>
     <link rel="stylesheet" href="//unpkg.com/bootstrap@4/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/layout.css">
+    <link rel="stylesheet" href="/css/layout.css" type="text/css">
     <script src="/js/android.js" type="text/javascript"></script>
 </head>
 <body>
     <div class="wrapper">
         <header>
-            <div class="header-section1" style="height: 33%; background-color: #000">
-                <!-- <a><img src="https://miro.medium.com/max/2000/0*as9CaTVWLBu4bntN.png" width:"150px" height="40px"></a> -->
-                <div class="nav-horizon">
-                    <ul>
-                        <li><a href="#">플랫폼</a></li>
-                        <li><a class="active" href="#">문서</a></li>
-                        <li><a href="#">더보기</a></li>                        
+        <div class="header-section1" style="height: 47px; background-color: #000000; margin: 0px; padding:0px">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light" style="height:47px; margin: 0px; padding:0px">
+                <a class="navbar-brand" href="#"><img src="https://miro.medium.com/max/2000/0*as9CaTVWLBu4bntN.png" width:"150px" height="47px"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active" style="border-bottom:4px solid greenyellow;">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider">
+                            </div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
                     </ul>
-                </div>
-                <!-- 검색-->
-                    <button class="btn dropdown-toggle" type="button" data-toggle="dropdown">LANGUAGE
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+                    </form>
+                    <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" style="float:right; border:1px solid #333">LANGUAGE
                     <span class="caret"></span></button>
                     <ul class="dropdown-menu" style="overflow-y:scroll;">
                         <li><a href="#">Language</a></li>
@@ -32,36 +58,68 @@
                         <li><a href="#">Deutsch</a></li>
                         <li><a href="#">English</a></li>
                     </ul>
-                    <div class="login">login</div>
-            </div>
-            <div class="header-section2" style="height: 33%; background-color: #fff">문서</div>
-            <div class="nav-horizon" style="height: 33%; background-color: #444">
-                <ul>
-                    <li><a href="#">개요</a></li>
-                    <li><a href="#">가이드</a></li>
-                    <li><a class="active" href="#">참조</a></li>
-                    <li><a href="#">샘플</a></li>
-                    <li><a href="#">디자인 및 품질</a></li>
-                </ul>
-            </div>
-        </header>
-        <nav style="overflow-y:scroll;">
+                    <div class="login" style="margin-left:10px; margin-right:10px;">login</div>
+                    </div>
+            </nav> 
+        </div>
+        <div class="header-section2" style="height: 47px; background-color: #eee;">문서</div>
+        <div class="nav-horizon" style="height: 47px; background-color: #444;">
             <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-                <li>7</li>
-                <li>8</li>
-                <li>9</li>
+                <li><a href="#">개요</a></li>
+                <li><a href="#">가이드</a></li>
+                <li><a class="active" href="#">참조</a></li>
+                <li><a href="#">샘플</a></li>
+                <li><a href="#">디자인 및 품질</a></li>
             </ul>
+        </div>
+    </header>
+    <nav class="nav-side" style="overflow-y:scroll;">
+        <ul>
+            <li><a href="#">1</a></li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+        </ul>
 
-        </nav>
-        <section></section>
-        <aside></aside>
-        <footer></footer>
-    </div>
+    </nav>        
+    <section>
+        <div class="article"></div>
+
+    </section>
+    <aside></aside>
+    <footer></footer>
+</div>
 </body>
 </html>
